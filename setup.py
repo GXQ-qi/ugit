f0fa0f378f6add46397118e2acf52e19027f2f36
guoxueqi <guoxueqi06@163.com>
2025-08-14 18:00:17 +0800

    学习了代码，加深了印象，也许放开一点，专注于思考而不是编写代码本身更加舒服更加，嗯，清晰一点

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 
-from setuptools inport setup
+from setuptools import setup
 
 setup (name = 'ugit',
-       version = '0.1',
+       version = '0.3',
        packages = ['ugit'],
        entry_points = {
            'console_scripts' : [
-               'ugit' = ugit.cli:main'
+               'ugit = ugit.cli:main'
                ]
             })
